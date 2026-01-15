ce03be3983c6bb70bb172724bad0df01cb69e766
wdx <1136346879@qq.com>
2026-01-15 14:28:19 +0800

    AgreementH5Page加载白屏问题处理

--- lib/pages/agreement_h5_page.dart
+++ lib/pages/agreement_h5_page.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:yimabao/api/db_api.dart';
 import 'package:yimabao/config/project_config.dart';
 import 'package:yimabao/config/project_style.dart';
@@ -21,21 +19,16 @@ class AgreementH5Page extends StatefulWidget {
 }
 
 class _AgreementH5PageState extends State<AgreementH5Page> {
-  late WebViewController _controller;
+  InAppWebViewController? _controller;
   late String filePath;
+  double progress = 0;
 
   @override
   void initState() {
     super.initState();
     filePath = widget.index == 0 ? 'https://yimabao.cn/readtemplate/user-agreement' : 'https://yimabao.cn/readtemplate/privacy-policy';
-    _initController();
   }
 
-  _initController() {
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(filePath));
-  }
   @override
   void dispose() {
     super.dispose();
@@ -74,7 +67,53 @@ class _AgreementH5PageState extends State<AgreementH5Page> {
         ],
       ),
       body: Container(
-        child: WebViewWidget(controller: _controller),
+        color: Colors.white,
+        child: Stack(
+          children: [
+            InAppWebView(
+              initialUrlRequest: URLRequest(url: WebUri(filePath)),
+              initialSettings: InAppWebViewSettings(
+                isInspectable: kDebugMode,
+                mediaPlaybackRequiresUserGesture: false,
+                allowsInlineMediaPlayback: true,
+                iframeAllowFullscreen: true,
+                useHybridComposition: true,
+                domStorageEnabled: true,
+                databaseEnabled: true,
+                clearCache: true,
+                safeBrowsingEnabled: true,
+                mixedContentMode: MixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
+              ),
+              onWebViewCreated: (controller) {
+                _controller = controller;
+              },
+              onReceivedServerTrustAuthRequest: (controller, challenge) async {
+                // 忽略 SSL 证书错误，解决证书过期或不被信任导致的白屏问题
+                return ServerTrustAuthResponse(action: ServerTrustAuthResponseAction.PROCEED);
+              },
+              onLoadStart: (controller, url) {
+                debugPrint("WebView started loading: $url");
+              },
+              onLoadStop: (controller, url) async {
+                debugPrint("WebView finished loading: $url");
+              },
+              onReceivedError: (controller, request, error) {
+                debugPrint("WebView error: ${error.description}, code: ${error.type}");
+              },
+              onProgressChanged: (controller, progress) {
+                setState(() {
+                  this.progress = progress / 100;
+                });
+              },
+            ),
+            progress < 1.0
+                ? LinearProgressIndicator(value: progress, color: Colors.amber,)
+                : Container(
+              height: 2,
+              color: Colors.red
+            ),
+          ],
+        ),
       ),
     );
   }
